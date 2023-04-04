177a319b2c60060584cb9eb790adf75d5be6a0ae
Taneristique <grkmtnrackz@outlook.fr>
2023-04-04 20:47:58 +0300

    metamask connection

---
+++ src/Connect.js
@@ -0,0 +1,18 @@
+let userAccount=0;
+const {ethers}=require("ethers");
+let provider;
+export async function connectWallet(){
+     provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    // Prompt user for account connections
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    userAccount=await signer.getAddress();
+    console.log("Account:", await signer.getAddress());
+    return userAccount;
+}
+export const test=()=>{
+    return "This was a  test nothing more";
+}
+
+
+
