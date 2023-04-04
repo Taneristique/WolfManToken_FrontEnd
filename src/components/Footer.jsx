e17ec9877f8e5baeb611a652ef9bd80bbf5a0d79
Taneristique <grkmtnrackz@outlook.fr>
2023-04-04 20:50:31 +0300

    components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../input.css";
+import React, { Component } from "react";
+export default class Footer extends Component{
+        render(){
+            return(
+              <footer className="bg-green-300	text-center text-green-600 fixed bottom-0 w-full">
+                     <p>This App Developped By Taneristique</p>
+              </footer>
+           
+            )
+        }
+   }
---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React,{Component} from "react";
+import {Link} from 'react-router-dom'
+import {connectWallet} from "../Connect";
+
+export default class Header extends Component{
+  constructor(){
+    super();
+    this.state={check:false, userAddress : 'connect'};
+    if(localStorage.getItem("checkConnection")){ 
+      /*If any user connected show the latest connected address.
+        This condition will use to show user address as independent of
+        the page refreshments grace to local storage values.
+      */
+      if(!localStorage["User"]){/*If check is true User must exist. 
+      Set User to a default value if it does not exist*/
+        localStorage["User"]="connect";
+      }
+      this.setState({check:localStorage["checkConnection"]});
+      this.setState({userAddress : localStorage.getItem("User")});
+    }
+  }  
+  
+  render(){
+        return(
+          <>
+            <ul className="bg-slate-900 from-cyan-50 text-violet-50 block text-center h-10 ">
+                <li className="inline-block rounded-xl  shadow-lg shadow-green-500 w-20 md:shadow-xl relative top-1 right-2"><Link to="/">Store</Link></li>
+                <li className="inline-block rounded-xl shadow-lg shadow-green-500 w-20 md:shadow-xl relative top-1"><Link to="/account">Account</Link></li>
+                <button className="connectionButton bg-green-300 text-slate-800 content-none  rounded-xl object-cover inline-block relative top-1 left-2  shadow-lg shadow-green-500 md:shadow-xl text-ellipsis overflow-hidden ..." onClick={
+                  async()=>{
+                    if(this.state.check===false){
+                      /*user will connect to metamask if user was not connected 
+                        else same connection will be last but in both cases user
+                        address will be shown on the button.
+                      */
+                       localStorage.setItem("checkConnection",true);
+                       localStorage.setItem("User", await connectWallet());
+                       this.setState({check : true});
+                       this.setState({userAddress : localStorage["User"]});
+                      }
+                    else{
+                      /*Button won't show user address.*/
+                        localStorage.setItem("User","connect");
+                        this.setState({userAddress : localStorage["User"]});
+                        localStorage.setItem("checkConnection",false);
+                      }}
+                  }>{localStorage["checkConnection"]?localStorage["User"]:this.state.userAddress}</button>
+            </ul>
+          </>
+        );
+    }}
